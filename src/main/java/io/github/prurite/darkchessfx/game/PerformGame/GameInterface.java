5db2f594040c601abd95c9e428945f3400d6f3a4
prurite <prurite@outlook.com>
2022-12-18 16:16:23 +0800

    Change GameInterface

--- src/main/java/io/github/prurite/darkchessfx/game/PerformGame/GameInterface.java
+++ src/main/java/io/github/prurite/darkchessfx/game/PerformGame/GameInterface.java
@@ -3,43 +3,20 @@ package io.github.prurite.darkchessfx.game.PerformGame;
 import io.github.prurite.darkchessfx.game.UserData.Player;
 
 public interface GameInterface {
-    State getState();
-
-    //public Game(String path); // constructor
-    public PlayerInGame[] startGame(Player p1, Player p2);
-    public String clickOnChess(PlayerInGame p, int x, int y);
-    public boolean checkEndGame();
-    public PlayerInGame getWinner();
-    // endGame, calculate gameCount, scoredGameCount, winCount, and playing time, calll the showResultWindow
-    public void endGame();
-    public void setUnscored(); // haven't implemented
-    public PlayerInGame qryCur(); // return the current turn player
-    public Piece[][] getChessboard();
-    public EatenPieces getEatenPieces();
-    public void saveGame();// haven't implemented
-    public void loadGame();// haven't implemented
-    public Move withdraw(); // return last move
-    public void updGUI(int x,int y);
-    // {
-        // need to update both chessboard and eaten Pieces
-    //}
-    public void updGUI(Move move);
-    // {
-//        updGUI(move.getCurx(), move.getCury());
-//        updGUI(move.getNewx(), move.getNewy());
-//    }
-    public void updEndGUI();
-//     {
-//        // need to update both chessboard and eaten Pieces
-//    }}
-    public String qryPiece(int x, int y); // format as "General-BLACK"
-    /*
-    public boolean FirstMoveFlag;
-    is deefined, with this you can know if this is the first turn (and you need to update side of each of the players)
-     */
-     public void pause(); // haven't checked if this is not faulty
-     public void resume(); // haven't checked if this is not faulty
-
-     public String debugChessboard(); // return status of the all board
-
+    public Game();
+    public Game(GameConfig config);
+    public Piece[][] getChessBoard();
+    public Side getPlayerSide(PlayerInGame p);
+    public Arraylist<Piece> getCaptured(Player p);
+    public int getScore(Player p);
+    public Arraylist<Pair> getPossibleMoves(Pair pos);
+    public String performMove(PlayerInGame p, Move move);
+    // Skipping a turn due to timeout is handled by GUI
+    public String loadGame(String path);
+    public String saveGame(String path);
+    public int getCurrentMovePos(); // -1 first, 0 mid, 1 last
+    public int goToPrevMove(); // Return current move id
+    public int goToNextMove();
+    public GameConfig getGameConfig();
+    public Player getWinner(); // null if not ended
 }
