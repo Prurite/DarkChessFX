21b5b083c8194a33de471724a795a7cd85e1bccd
prurite <prurite@outlook.com>
2022-12-20 10:14:26 +0800

    Update PlayerInfoProcessorInterface

--- src/main/java/io/github/prurite/darkchessfx/model/PlayerInfoProcessorInterface.java
+++ src/main/java/io/github/prurite/darkchessfx/model/PlayerInfoProcessorInterface.java
@@ -1,5 +1,10 @@
 package io.github.prurite.darkchessfx.model;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface PlayerInfoProcessorInterface {
     public Player getPlayer(String name);
+    public void readFromFile(File file) throws IOException;
+    public void saveToFile(File file) throws IOException;
 }
